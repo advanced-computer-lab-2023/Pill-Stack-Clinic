4625d1d493f7ba5b6a792d1d5716d5abeb3c8955
Omar Azzam <omarhossam20@gmail.com>
2023-11-10 15:39:52 +0200

    Doctor Unsigned Home Added

--- frontend/src/Components/Pages/DoctorUnsigned.jsx
+++ frontend/src/Components/Pages/DoctorUnsigned.jsx
@@ -1,48 +1,183 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
+import { Box, Flex, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import UnsignedDoctorShortcuts from "../UI/UnsignedDoctorShortcuts";
+import DoctorInfoCard from "../UI/DoctorInfoCard";
+import WithSubnavigation from "../UI/navbar";
+
 
 export const UnsignedDoctor = () => {
   const navigate = useNavigate();
   const [cookies, removeCookie] = useCookies([]);
   const [username, setUsername] = useState("");
+  const [doctorData, setDoctorData] = useState([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = useRef(null);
+  const finalRef = useRef(null);
+
   useEffect(() => {
     const verifyCookie = async () => {
       if (!cookies.token) {
-        navigate("/");
+        navigate("/login");
       }
-      const { data } = await axios.post(
-        "http://localhost:8000",
-        {},
-        { withCredentials: true }
-      );
+      const { data } = await axios.post("http://localhost:8000", {}, { withCredentials: true });
       const { status, user } = data;
       setUsername(user);
       return status
         ? toast(`Hello ${user}`, {
             position: "top-right",
           })
-        : (removeCookie("token"), navigate("/"));
+        : (removeCookie("token"), navigate("/login"));
     };
     verifyCookie();
+
+    // Fetch the doctor's data
+    const getDoctorData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/doctor/profile", {
+          withCredentials: true,
+        });
+        setDoctorData(response.data);
+      } catch (error) {
+        console.error("Error fetching doctor data:", error);
+      }
+    };
+
+    getDoctorData();
   }, [cookies, navigate, removeCookie]);
+
+  useEffect(() => {
+    // Check if doctorData is not null and log it
+    if (doctorData !== null) {
+      console.log("Doctor Data:", doctorData);
+    }
+  }, [doctorData]);
+
   const Logout = () => {
     removeCookie("token");
-    navigate("/");
+    navigate("/signup");
   };
+
+  const handleChangePass = async () => {
+    const oldPassword = document.querySelector('#oldPassword').value;
+    const newPassword = document.querySelector('#newPassword').value;
+    const confirmNewPassword = document.querySelector('#confirmNewPassword').value;
+  
+    if (newPassword !== confirmNewPassword) {
+      toast.error('Passwords dont match', {
+        position: 'top-right',
+        autoClose: 3000,
+      });
+      return;
+    }
+  
+    const data = {
+      oldPassword: oldPassword,
+      newPassword: newPassword,
+    };
+  
+    try {
+      const response = await axios.post('http://localhost:8000/changePassword', data, {
+        withCredentials: true,
+      });
+  
+      if (response.status === 201) {
+        toast.success(response.data.message, {
+          position: 'top-right',
+          autoClose: 3000,
+        });
+      } else {
+        toast.error(response.data.message, {
+          position: 'top-right',
+          autoClose: 3000,
+        });
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+      // Log the detailed error response for debugging
+      if (error.response) {
+        console.error('Response Data:', error.response.data);
+        toast.error(error.response.data.message, {
+          position: 'top-right',
+          autoClose: 3000,
+        });
+      }
+    }
+  };
+
   return (
     <>
-      <div className="doctorHome">
+      <WithSubnavigation username={doctorData.Name} onOpenModal={onOpen} />
+      <div className="home_page" style={{ background: "linear-gradient(45deg, #1E9AFE, #60DFCD)" }}>
         <h4>
-          {" "}
-          Welcome Unsigned <span>{username}</span>
+          Welcome Unsigned Docc {doctorData[0]?.Email} <span>{username}</span>
         </h4>
+        <Flex>
+          <Box style={{ margin: "0 10px", flex: 1 }}>
+            <DoctorInfoCard
+              title={`Masa2 el anwar ya ${username}`}
+              username={doctorData.Username}
+              name={doctorData.Name}
+              email={doctorData.Email}
+              DateOfBirth={doctorData.DateOfBirth}
+              HourlyRate={doctorData.HourlyRate}
+              affiliation={doctorData.Affiliation}
+              EducationalBackground={doctorData.EducationalBackground}
+              WalletBalance={doctorData.WalletBalance}
+              id={doctorData._id}
+            />
+          </Box>
+
+          <Box style={{ margin: "0 10px", flex: 1 }}>
+            <UnsignedDoctorShortcuts style={{ height: "100%" }} />
+          </Box>
+        </Flex>
+
         <button onClick={Logout}>LOGOUT</button>
+
+        {/* <DrawerExample isOpen={true} /> Set isOpen prop to true */}
+        <Modal
+          initialFocusRef={initialRef}
+          finalFocusRef={finalRef}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Change Password</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Old Password</FormLabel>
+                <Input id="oldPassword" type="password" ref={initialRef} placeholder="Old Password" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>New Password</FormLabel>
+                <Input id="newPassword" type="password" placeholder="New Password" />
+              </FormControl>
+
+              <FormControl mt={4}>
+                <FormLabel>Confirm Password</FormLabel>
+                <Input id="confirmNewPassword" type="password" placeholder="Confirm New Password" />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button onClick={handleChangePass} colorScheme="blue" mr={3}>
+                Save
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </div>
       <ToastContainer />
     </>
   );
 };
 
+export default UnsignedDoctor;
---
+++ frontend/src/Components/UI/UnsignedDoctorShortcuts.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Card, CardHeader, CardBody } from '@chakra-ui/react';
+import { Heading, Stack, StackDivider, Box, Text} from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react'
+import { Divider } from '@chakra-ui/react'
+import { Center,AbsoluteCenter , Square, Circle } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
+import { DeleteIcon, AddIcon, EmailIcon ,EditIcon,ViewIcon,AttachmentIcon} from '@chakra-ui/icons'
+import { Link } from 'react-router-dom'; // Import the Link component
+import { LinkBox, LinkOverlay } from '@chakra-ui/react'
+import { useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+
+
+function DoctorShortcuts(props) {
+  const {openSecondModal,openAddFamilyModal,openViewFamilyModal,openUploadDocModal, navigate, username} = props;
+  
+  
+  return (
+    <Card>
+<CardHeader h='80px'>
+          <Heading size='md'>Shortcuts</Heading>
+          </CardHeader>
+<Grid
+  h='150px'
+  w='197px'  
+  templateRows='repeat(2, 1fr)'
+  templateColumns='repeat(2, 1fr)'  
+  gap={1}
+>
+<LinkBox as='article' w='197px' h='148px' maxW='sm' p='1' borderWidth='1px' rounded='md' href="doctor-home/contract"
+_hover={{ bg: '#353535',  color: 'white' , cursor: "pointer"}}>
+  <LinkOverlay href="doctor-home/contract"></LinkOverlay>
+  <AbsoluteCenter>
+    
+      <Box fontSize="16px" mb="10px">Contract</Box>
+    
+    <Center>
+      <a href="doctor-home/contract" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <ViewIcon boxSize={5} />
+       
+      </a>
+    </Center>
+  </AbsoluteCenter>
+</LinkBox>
+
+
+
+
+
+
+
+</Grid>
+</Card>
+  );
+}
+
+export default DoctorShortcuts;
